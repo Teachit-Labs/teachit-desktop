bd1e15c7e02db3e5348b429a3c84f7943647d988
Simon Bjørnøy <simon.bjornoy@gmail.com>
2024-10-08 19:02:02 +0200

    feat: add first atoms

---
+++ app/components/atoms/typescale.ts
@@ -0,0 +1,56 @@
+// styles are exported to tailwind.config.ts
+
+import { KeyValuePair } from "tailwindcss/types/config";
+
+export default {
+  "5xl": [
+    "3.831rem",
+    { lineHeight: "5rem", letterSpacing: "0", fontWeight: "regular" },
+  ],
+  "4xl": [
+    "3.063rem",
+    { lineHeight: "4.5rem", letterSpacing: "0", fontWeight: "regular" },
+  ],
+  "3xl": [
+    "2.438rem",
+    { lineHeight: "3.5rem", letterSpacing: "0", fontWeight: "regular" },
+  ],
+  "2xl": [
+    "1.938rem",
+    { lineHeight: "3rem", letterSpacing: "0", fontWeight: "regular" },
+  ],
+  xl: [
+    "1.563rem",
+    { lineHeight: "2rem", letterSpacing: "0", fontWeight: "regular" },
+  ],
+  lg: [
+    "1.313rem",
+    { lineHeight: "2rem", letterSpacing: "0", fontWeight: "regular" },
+  ],
+  md: [
+    "1.125rem",
+    { lineHeight: "2rem", letterSpacing: "0", fontWeight: "regular" },
+  ],
+  sm: [
+    "1rem",
+    { lineHeight: "1.5rem", letterSpacing: "0", fontWeight: "regular" },
+  ],
+  xs: [
+    "0.875rem",
+    { lineHeight: "1.5rem", letterSpacing: "0", fontWeight: "regular" },
+  ],
+  subheading: [
+    "0.875rem",
+    { lineHeight: "1.5rem", letterSpacing: "5%", fontWeight: "medium" },
+  ],
+} as KeyValuePair<
+  string,
+  [
+    fontSize: string,
+    configuration: {
+      lineHeight: string;
+      letterSpacing: string;
+      fontWeight: string | number;
+    }
+  ]
+>;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,25 @@
 import type { Config } from "tailwindcss";
 
+import color from "./app/components/atoms/color";
+import typescale from "./app/components/atoms/typescale";
+
 export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          '"Inter"',
-          "ui-sans-serif",
-          "system-ui",
-          "sans-serif",
-          '"Apple Color Emoji"',
-          '"Segoe UI Emoji"',
-          '"Segoe UI Symbol"',
-          '"Noto Color Emoji"',
-        ],
-      },
+    colors: color,
+    fontSize: typescale,
+    fontFamily: {
+      sans: [
+        "'Work Sans'",
+        '"Inter"',
+        "ui-sans-serif",
+        "system-ui",
+        "sans-serif",
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
     },
   },
   plugins: [],
