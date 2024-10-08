1256eb776d26120a2cc78c87f7ccb780479303d3
Simon Bjørnøy <simon.bjornoy@gmail.com>
2024-10-08 22:14:06 +0200

    feat: login-page

---
+++ app/components/molecules/button.ts
@@ -0,0 +1,14 @@
+import { tv } from "tailwind-variants";
+
+export const button = tv({
+  base: "px-4 py-2 text-sm font-normal rounded-lg transition-all duration-150",
+  variants: {
+    color: {
+      primary:
+        "bg-primary-500 text-white hover:bg-primary-700 hover:text-secondary-200 hover:shadow-sm",
+    },
+  },
+  defaultVariants: {
+    color: "primary",
+  },
+});
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,11 @@
 import { Link } from "@remix-run/react";
-import { Button } from "~/components/molecules/button";
+import { button } from "~/components/molecules/button";
+import { input } from "~/components/molecules/input";
 
 export default function Index() {
   return (
     <div className="inset-0 bg-primary-300 h-screen w-screen flex items-center antialiased">
-      <div className="flex flex-col gap-8 px-4 items-start w-full">
+      <div className="flex flex-col gap-8 px-4 items-start w-full ml-32">
         <div className="bg-primary-400 px-8 py-2 inline ">Logo goes here</div>
         <h1 className="font-display tracking-wider text-5xl">
           Velkommen til Teachit{" "}
@@ -13,16 +14,51 @@ export default function Index() {
       <div className="flex items-center justify-center w-full h-full">
         <div className="bg-white text-black p-10 shadow-md rounded-lg flex flex-col gap-6">
           <h2 className="text-xl mb-2">Logg inn</h2>
-          <Button>Logg inn med facebook</Button>
-          <Button>Logg inn med google</Button>
-          <Button>Logg inn med apple</Button>
-          <div className="px-24 py-4">eller logg inn med e-post</div>
-          <input type="text" name="" id="" />
-          <input type="text" name="" id="" />
-          <Link to="#">Glemt passord?</Link>
-          <Button>Logg inn</Button>
+          <button className={button()}>Logg inn med facebook</button>
+          <button className={button()}>Logg inn med google</button>
+          <button className={button()}>Logg inn med apple</button>
+          <div className="py-4 flex items-center gap-2">
+            <span className="bg-neutral-200 h-[1px] w-20" />
+            eller logg inn med e-post
+            <span className="bg-neutral-200 h-[1px] w-20" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email">E-post</label>
+            <input
+              className={input()}
+              placeholder="bruker@epost.bo"
+              type="text"
+              name="email"
+              id="email"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password">Passord</label>
+            <input
+              className={input()}
+              type="password"
+              placeholder="******"
+              name="password"
+              id="password"
+            />
+          </div>
+          <Link
+            className="text-primary-600 underline underline-offset-2"
+            to="#"
+          >
+            Glemt passord?
+          </Link>
+          <button className={button({ className: "max-w-48" })}>
+            Logg inn
+          </button>
           <div>
-            Trenger du en konto? <Link to="#">Registrer deg</Link>
+            Trenger du en konto?{" "}
+            <Link
+              className="text-primary-600 underline underline-offset-2"
+              to="#"
+            >
+              Registrer deg
+            </Link>
           </div>
         </div>
       </div>
