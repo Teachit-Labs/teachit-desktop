ef428eac4ed557213656c816ba65a7375ecc313a
Simon Bjørnøy <simon.bjornoy@gmail.com>
2024-10-08 19:16:08 +0200

    feat: add box-shadow and blur

---
+++ app/components/atoms/shadow.ts
@@ -0,0 +1,12 @@
+export default {
+  "box-shadow": {
+    lg: "0px 32px 74px -12px rgba(16, 24, 40, 0.2)",
+    md: "0px 24px 48px -12px rgba(16, 24, 40, 0.25)",
+    sm: "0px 8px 8px -4px rgba(16, 24, 40, 0.04)",
+  },
+  blur: {
+    lg: "32px",
+    md: "16px",
+    sm: "8px",
+  },
+};
